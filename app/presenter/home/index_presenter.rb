2016300efba04ddec1037e31598d1bc3a1c3a810
Bodhish Thomas <bodhish@gmail.com>
2019-08-11 06:15:47 +0530

    Fix a bug in index presenter

--- app/presenter/home/index_presenter.rb
+++ app/presenter/home/index_presenter.rb
@@ -1,7 +1,7 @@
 module Home
   class IndexPresenter < ::ApplicationPresenter
     def props
-      Message.all.includes(:page).each do |m|
+      Message.all.includes(:page).map do |m|
         {
           message: m.message,
           page_name: m.page.name
